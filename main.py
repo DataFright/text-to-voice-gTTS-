ed9a86b29b10337f364fcd0a1b285fe3a69f35a4
Matthew Swaney <Matthew.J.Swaney@gmail.com>
2023-11-08 19:12:59 -0600

    update

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import os
-import pygame
 from gtts import gTTS
+import pygame
 from time import sleep
 
-# Initialize the mixer module
-pygame.mixer.init()
+
+# Function to split the text into paragraphs
+def split_in_paragraphs(text):
+    return [para.strip() for para in text.split('\n\n') if para.strip()]
+
+
+# Function to save a paragraph as speech to a file
+def save_speech_to_file(part_text, part_number, language='en', cache_dir='tts_cache'):
+    part_mp3_file = os.path.join(cache_dir, f"output_paragraph_{part_number}.mp3")
+    tts = gTTS(text=part_text, lang=language, slow=False)
+    tts.save(part_mp3_file)
+    return part_mp3_file
+
 
 # Define the path to the text file you want to read
 text_file_path = 'the_text.txt'
@@ -16,10 +28,6 @@ with open(text_file_path, 'r', encoding='utf-8') as file:
 # Language in which you want to convert
 language = 'en'
 
-# Function to split the text into paragraphs
-def split_in_paragraphs(text):
-    return [para.strip() for para in text.split('\n\n') if para.strip()]
-
 # Splitting the text into paragraphs
 paragraphs = split_in_paragraphs(mytext)
 
@@ -27,31 +35,51 @@ paragraphs = split_in_paragraphs(mytext)
 cache_dir = 'tts_cache'
 os.makedirs(cache_dir, exist_ok=True)
 
-# Function to get a unique filename for a paragraph
-def get_cache_filename(text):
-    # Here we just use a simple hash, but you might want more collision-resistant approach
-    return f"{hash(text)}.mp3"
 
-# Process each paragraph
-for part_number, part_text in enumerate(paragraphs):
-    cache_filename = get_cache_filename(part_text)
-    cache_filepath = os.path.join(cache_dir, cache_filename)
+# Process paragraphs using ThreadPoolExecutor
+def generate_audio_files(paragraphs, language):
+    with ThreadPoolExecutor() as executor:
+        # Create a future to part number mapping
+        future_to_part = {executor.submit(save_speech_to_file, para, idx, language, cache_dir): idx for idx, para in
+                          enumerate(paragraphs)}
+        # As each future completes, print out a confirmation message
+        for future in as_completed(future_to_part):
+            part_number = future_to_part[future]
+            try:
+                # Wait for the audio file to be saved and get the filename
+                part_mp3_file = future.result()
+                print(f"Paragraph {part_number + 1} audio saved as {part_mp3_file}")
+            except Exception as exc:
+                print(f"Paragraph {part_number + 1} generated an exception: {exc}")
 
-    # If we've already generated this paragraph before, use the cached file
-    if not os.path.exists(cache_filepath):
-        tts = gTTS(text=part_text, lang=language, slow=False)
-        tts.save(cache_filepath)
 
-    # Print the part number and the text being read to the terminal
-    print(f"Reading paragraph {part_number + 1}/{len(paragraphs)}...\n")
-    print(part_text)
+# Call the function to generate audio files
+generate_audio_files(paragraphs, language)
 
-    # Load the paragraph
-    pygame.mixer.music.load(cache_filepath)
+# Initialize the mixer module
+pygame.mixer.init()
 
-    # Play the paragraph
-    pygame.mixer.music.play()
 
-    # Wait for playback to finish for this paragraph
-    while pygame.mixer.music.get_busy():
-        sleep(1)
+# Function to play the generated audio files
+def play_audio_files(paragraphs, cache_dir='tts_cache'):
+    for part_number, part_text in enumerate(paragraphs):
+        part_mp3_file = os.path.join(cache_dir, f"output_paragraph_{part_number}.mp3")
+        if os.path.exists(part_mp3_file):
+            # Print the part number and the text being read to the terminal
+            print(f"Reading paragraph {part_number + 1}/{len(paragraphs)}...\n")
+            print(part_text)  # Print the text of the current paragraph
+
+            # Load the audio file and play it
+            pygame.mixer.music.load(part_mp3_file)
+            pygame.mixer.music.play()
+
+            # Wait for playback to finish for this paragraph
+            while pygame.mixer.music.get_busy():
+                sleep(1)
+
+
+# Call the function to play audio files
+play_audio_files(paragraphs, cache_dir)
+
+# Remember to quit pygame when done
+pygame.quit()
